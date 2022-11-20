acfad294e0148a3407f474950120d0310b5db142
Shafeenes <59567149+Shafeenes@users.noreply.github.com>
2022-11-20 16:55:43 +0300

    ss

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -33,9 +33,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "7e0ea4cee35d0bcc10cfc4245f70df22",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "085182bf6bce94df3295d350cd47fe71",
-"/": "085182bf6bce94df3295d350cd47fe71",
-"main.dart.js": "fe0686f8491047cdf91255141fbe4a0f",
+"index.html": "9aae4c37bbe4be23a9b33e33e371614b",
+"/": "9aae4c37bbe4be23a9b33e33e371614b",
+"main.dart.js": "b8eee0f9fdbe116b29dd91ff23db9e0f",
 "manifest.json": "56128fc6ec7568b99d6fc24cfa34a218",
 "OneSignalSDKWorker.js": "ebb63ca15bba16b550232b0b0f66c726",
 "version.json": "b8c9a0377c450d553a68d576bb0cad90"
